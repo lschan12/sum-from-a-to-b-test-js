ddee43371aeb72178203369650ac4a20ef5c0c7c
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-08-26 00:03:23 -0700

    implemented a recursive function that takes in two numbers and sums all the numbers in between.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  if (fromN === toN) {
+    return fromN;
+  }
+  return fromN + sum(fromN + 1, toN);
+};
 
-module.exports = sum;
+console.log(sum(3, 7));
+
+// module.exports = sum;
